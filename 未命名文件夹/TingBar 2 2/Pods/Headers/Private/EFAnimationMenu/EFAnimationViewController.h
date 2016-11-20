19cab383673f745b6e94d7b5a1fe99bbf2feb55d
Cong Wang <congwang@CongdeMacBook-Pro.local>
2016-11-20 16:31:26 +0800

    first commit

--- 未命名文件夹/TingBar 2 2/Pods/Headers/Private/EFAnimationMenu/EFAnimationViewController.h
+++
@@ -1,1 +0,0 @@
-../../../EFAnimationMenu/EFAnimationMenu/EFAnimationViewController.h
--- 未命名文件夹/TingBar 2 2/Pods/Target Support Files/EFAnimationMenu/EFAnimationMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EFAnimationMenu : NSObject
-@end
-@implementation PodsDummy_EFAnimationMenu
-@end
